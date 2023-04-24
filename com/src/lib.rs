316b5d25fcf9a33f9dffbaf36f0a1e9946b5e57e
Adil Haidar <adilhaidar00@gmail.com>
2023-04-24 21:43:55 +0300

    Add command status check

--- com/src/lib.rs
+++ com/src/lib.rs
@@ -5,13 +5,18 @@ use std::thread::spawn;
 use std::time::Duration;
 
 use mavlink::*;
-use mavlink::ardupilotmega::COMMAND_LONG_DATA;
-use mavlink::ardupilotmega::CopterMode::COPTER_MODE_GUIDED;
+use mavlink::ardupilotmega::MavCmd::{self, *};
+use mavlink::ardupilotmega::{COMMAND_LONG_DATA, MavResult};
+use mavlink::ardupilotmega::CopterMode::{COPTER_MODE_GUIDED, COPTER_MODE_LAND};
+
 use mavlink::error::MessageWriteError;
-use mavlink::ardupilotmega::MavCmd;
 
 pub use mavlink::ardupilotmega::MavMessage;
+use mavlink::ardupilotmega::MavResult::MAV_RESULT_DENIED;
 pub use mavlink::MavlinkVersion as Mavlink;
+
+use mavlink::common::MavMode::MAV_MODE_STABILIZE_ARMED;
+use mavlink::common::MavResult::{MAV_RESULT_ACCEPTED, MAV_RESULT_FAILED};
 
 pub struct MavlinkPi {
     vehicle: Arc<Mutex<Connection>>,
@@ -26,12 +31,21 @@ pub enum Speed {
 
 type Connection = Box<dyn MavConnection<MavMessage> + Sync + Send>;
 type MavRequest = Result<usize, MessageWriteError>;
-type MavResponse = Result<(MavHeader, MavMessage), Box<dyn Error>>;
+type MavResponse = Result<MavResult, Box<dyn Error + 'static>>;
 
 const TIMEOUT: Duration = Duration::from_secs(10);
 
 #[derive(Debug, Clone)]
 pub struct NoMavlinkDeviceFoundError;
+
+#[derive(Debug, Clone)]
+pub struct NoResponseError;
+
+#[derive(Debug, Clone)]
+pub struct Rejected;
+
+#[derive(Debug, Clone)]
+pub struct Failed;
 
 impl Display for NoMavlinkDeviceFoundError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
@@ -41,45 +55,54 @@ impl Display for NoMavlinkDeviceFoundError {
 
 impl Error for NoMavlinkDeviceFoundError {}
 
+impl Display for NoResponseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("No response")
+    }
+}
+
+impl Error for NoResponseError {}
+
+impl Display for Rejected {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Command rejected")
+    }
+}
+
+impl Error for Rejected {}
+
+impl Display for Failed {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Failed")
+    }
+}
+
+impl Error for Failed {}
+
 impl MavlinkPi {
+    pub fn connect_sim(port: u32, version: MavlinkVersion) -> Result<MavlinkPi, Box<dyn Error>> {
+        let loc = format!("udpin:127.0.0.1:{}", port);
+
+        let mut connection = connect::<MavMessage>(&loc)?;
+        connection.set_protocol_version(version);
+        establish(connection)
+    }
+
     pub fn connect(serial_id: u8,
                    baud_rate: Speed,
                    version: MavlinkVersion) -> Result<MavlinkPi, Box<dyn Error>> {
         let loc = format!("serial:/dev/serial{serial_id}:{}", baud_rate as u32);
 
-        let mut vehicle = connect::<MavMessage>(&loc)?;
-        vehicle.set_protocol_version(version);
-
-        println!("Establishing the MAVLink {version:?} connection... ({TIMEOUT:?})");
-        Ok(MavlinkPi {
-            vehicle: Self::dial(Mutex::new(vehicle).into(),
-                                |connection| {
-                                    connection.lock().unwrap().recv().unwrap();
-                                    connection
-                                },
-            )?,
-            header: MavHeader::default(),
-        })
-    }
-
-    fn dial<F>(connection: Arc<Mutex<Connection>>,
-               action: fn(Arc<Mutex<Connection>>) -> F) -> Result<F, Box<dyn Error>>
-        where F: Sync + Send + 'static {
-        let (tx, rx) = mpsc::channel();
-
-        spawn(move || { tx.send(action(connection)) });
-        match rx.recv_timeout(TIMEOUT) {
-            Ok(payload) => Ok(payload),
-            Err(_) => Err(Box::try_from(NoMavlinkDeviceFoundError).unwrap())
-        }
+        let mut connection = connect::<MavMessage>(&loc)?;
+        connection.set_protocol_version(version);
+        establish(connection)
     }
 
     pub fn send(&self, msg: MavMessage) -> MavRequest {
         self.vehicle.lock().unwrap().send(&self.header, &msg)
-
     }
 
-    pub fn receive(&self) -> MavResponse {
+    pub fn receive(&self) -> Result<(MavHeader, MavMessage), Box<dyn Error>> {
         let (rtx, rrx) = mpsc::channel();
         let receiver = self.vehicle.clone();
 
@@ -89,14 +112,79 @@ impl MavlinkPi {
             Err(_) => Err(Box::try_from(NoMavlinkDeviceFoundError).unwrap())
         }
     }
-}
 
-pub fn guided() -> MavMessage {
-    MavMessage::SET_MODE(ardupilotmega::SET_MODE_DATA {
-        custom_mode: COPTER_MODE_GUIDED as u32,
-        target_system: 0,
-        base_mode: Default::default(),
-    })
+    pub fn stabilize_armed(&self) -> MavResponse {
+        self.command(MAV_CMD_DO_SET_MODE,
+                     &[MAV_MODE_STABILIZE_ARMED as i32 as f32, 0., 0., 0., 0., 0., 0.])
+    }
+
+    pub fn enter_guided(&self) -> MavResponse {
+        self.command(MAV_CMD_DO_SET_MODE,
+                     &[COPTER_MODE_GUIDED as i32 as f32, 0., 0., 0., 0., 0., 0.])
+    }
+
+    pub fn land(&self) -> MavResponse {
+        self.command(MAV_CMD_DO_SET_MODE,
+                     &[COPTER_MODE_LAND as i32 as f32, 0., 0., 0., 0., 0., 0.])
+    }
+
+    pub fn takeoff(&self, height: f32) -> MavResponse {
+        self.command(MAV_CMD_NAV_TAKEOFF,
+                     &[0., 0., 0., 0., 0., 0., height])
+    }
+
+    pub fn arm(&self) -> MavResponse {
+        self.command(MAV_CMD_COMPONENT_ARM_DISARM, &[1., 0., 0., 0., 0., 0., 0., ])
+    }
+
+    pub fn disarm(&self) -> MavResponse {
+        self.command(MAV_CMD_COMPONENT_ARM_DISARM, DEFAULT_PARAMS)
+    }
+
+    pub fn force_arm(&self) -> MavResponse {
+        self.command(MAV_CMD_COMPONENT_ARM_DISARM,
+                     &[1., 21996., 0., 0., 0., 0., 0., ])
+    }
+
+    pub fn force_disarm(&self) -> MavResponse {
+        self.command(MAV_CMD_COMPONENT_ARM_DISARM,
+                     &[0., 21996., 0., 0., 0., 0., 0., ])
+    }
+
+    fn command(&self, r#type: MavCmd, params: &[f32]) -> MavResponse {
+        let data = MavMessage::COMMAND_LONG(
+            COMMAND_LONG_DATA {
+                param1: params[0],
+                param2: params[1],
+                param3: params[2],
+                param4: params[3],
+                param5: params[4],
+                param6: params[5],
+                param7: params[6],
+                command: r#type,
+                target_system: 0,
+                target_component: 0,
+                confirmation: 0,
+            }
+        );
+        self.send(data)?;
+        loop {
+            let (_, resp) = self.receive()?;
+            match resp {
+                MavMessage::COMMAND_ACK(result) =>
+                    if result.command == r#type {
+                        if result.result as i32 == MAV_RESULT_ACCEPTED as i32 {
+                            return Ok(result.result);
+                        } else if result.result as i32 == MAV_RESULT_FAILED as i32 {
+                            return Err(Box::new(Failed));
+                        } else if result.result as i32 == MAV_RESULT_DENIED as i32 {
+                            return Err(Box::new(Rejected));
+                        }
+                    },
+                _ => ()
+            }
+        }
+    }
 }
 
 pub fn heartbeat() -> MavMessage {
@@ -110,77 +198,7 @@ pub fn heartbeat() -> MavMessage {
     })
 }
 
-pub fn arm() -> MavMessage {
-    MavMessage::COMMAND_LONG(
-        COMMAND_LONG_DATA {
-            param1: 1.0,
-            param2: 0.0,
-            param3: 0.0,
-            param4: 0.0,
-            param5: 0.0,
-            param6: 0.0,
-            param7: 0.0,
-            command: MavCmd::MAV_CMD_COMPONENT_ARM_DISARM,
-            target_system: 0,
-            target_component: 0,
-            confirmation: 0,
-        }
-    )
-}
-
-pub fn disarm() -> MavMessage {
-    MavMessage::COMMAND_LONG(
-        COMMAND_LONG_DATA {
-            param1: 0.0,
-            param2: 0.0,
-            param3: 0.0,
-            param4: 0.0,
-            param5: 0.0,
-            param6: 0.0,
-            param7: 0.0,
-            command: MavCmd::MAV_CMD_COMPONENT_ARM_DISARM,
-            target_system: 0,
-            target_component: 0,
-            confirmation: 0,
-        }
-    )
-}
-
-pub fn force_arm() -> MavMessage {
-    MavMessage::COMMAND_LONG(
-        COMMAND_LONG_DATA {
-            param1: 1.0,
-            param2: 21196.0,
-            param3: 0.0,
-            param4: 0.0,
-            param5: 0.0,
-            param6: 0.0,
-            param7: 0.0,
-            command: MavCmd::MAV_CMD_COMPONENT_ARM_DISARM,
-            target_system: 0,
-            target_component: 0,
-            confirmation: 0,
-        }
-    )
-}
-
-pub fn force_disarm() -> MavMessage {
-    MavMessage::COMMAND_LONG(
-        COMMAND_LONG_DATA {
-            param1: 0.0,
-            param2: 21196.0,
-            param3: 0.0,
-            param4: 0.0,
-            param5: 0.0,
-            param6: 0.0,
-            param7: 0.0,
-            command: MavCmd::MAV_CMD_COMPONENT_ARM_DISARM,
-            target_system: 0,
-            target_component: 0,
-            confirmation: 0,
-        }
-    )
-}
+const DEFAULT_PARAMS: &[f32] = &[0., 0., 0., 0., 0., 0., 0.];
 
 pub fn request_parameters() -> MavMessage {
     MavMessage::PARAM_REQUEST_LIST(
@@ -198,4 +216,29 @@ pub fn request_stream() -> MavMessage {
             message_id: 0,
         },
     )
+}
+
+fn dial<F>(connection: Arc<Mutex<Connection>>,
+           action: fn(Arc<Mutex<Connection>>) -> F) -> Result<F, Box<dyn Error>>
+    where F: Sync + Send + 'static {
+    let (tx, rx) = mpsc::channel();
+
+    spawn(move || { tx.send(action(connection)) });
+    match rx.recv_timeout(TIMEOUT) {
+        Ok(payload) => Ok(payload),
+        Err(_) => Err(Box::new(NoMavlinkDeviceFoundError))
+    }
+}
+
+fn establish(connection: Connection) -> Result<MavlinkPi, Box<dyn Error>> {
+    println!("Establishing a MAVLink connection... ({TIMEOUT:?})");
+    Ok(MavlinkPi {
+        vehicle: dial(Mutex::new(connection).into(),
+                      |connection| {
+                          connection.lock().unwrap().recv().unwrap();
+                          connection
+                      },
+        )?,
+        header: MavHeader::default(),
+    })
 }
---
+++ src/bin/mavlink_sim_test.rs
@@ -0,0 +1,47 @@
+use std::error::Error;
+use std::str::from_utf8;
+use std::thread::sleep;
+use std::time::Duration;
+
+use com::*;
+
+fn main_handler(msg: MavMessage) -> Result<(), Box<dyn Error>> {
+    Ok(match msg {
+        MavMessage::STATUSTEXT(status) =>
+            eprintln!("{}", from_utf8(status.text.into_iter()
+                .filter(|&c| c != 0).collect::<Vec<u8>>().as_slice())?),
+        MavMessage::COMMAND_ACK(result) => println!("{:?}", result),
+        MavMessage::HEARTBEAT(_) => (),
+        _ => ()
+    })
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    match MavlinkPi::connect_sim(14550, Mavlink::V2) {
+        Ok(vehicle) => {
+            println!("\nConnected");
+            vehicle.enter_guided().unwrap(); // TODO: won't work
+            sleep(Duration::from_millis(5));
+
+            // Arm and takeoff
+            println!("\nTakeoff");
+            vehicle.arm().unwrap();
+            sleep(Duration::from_millis(1000));
+            vehicle.takeoff(5.).unwrap();
+            sleep(Duration::from_millis(7000));
+
+            // Landing
+            println!("\nLanding");
+            vehicle.land()?;
+
+            loop {
+                let (_, data) = vehicle.receive()?;
+                main_handler(data)?;
+                sleep(Duration::from_millis(10));
+            }
+        }
+        Err(e) => { eprintln!("{e}") }
+    };
+
+    Ok(())
+}
