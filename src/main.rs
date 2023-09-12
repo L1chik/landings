4b267742fef2e6cee67661aca47a3cff74cf01ed
z.bykov.d <z.bykov.d@gmail.com>
2023-09-12 23:54:36 +0300

    Add mp4 debug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::time::Instant;
+use std::{path::PathBuf, time::Instant};
 
 use opencv::{
     calib3d::solve_pnp,
     calib3d::{draw_frame_axes, rodrigues, SOLVEPNP_IPPE_SQUARE},
-    core::{self, Vec3f, CV_32FC3},
-    highgui,
+    core::{self, Point3_, Vec3f, CV_32FC3},
+    // highgui,
     imgproc::{arrowed_line, put_text},
     objdetect::{
         draw_detected_markers, get_predefined_dictionary, ArucoDetector, DetectorParameters,
@@ -17,7 +17,10 @@ use opencv::{
     Result,
 };
 
+#[cfg(feature = "pi")]
 use rppal::uart::{Parity, Uart};
+
+use video_rs::{Encoder, EncoderSettings, Locator, Time};
 
 const M_LEN: f32 = 24.;
 const M_ID: i32 = 44;
@@ -26,10 +29,10 @@ const CDST: &[f32] = &[0.22, -0.66, 0., 0., 0.66];
 const CMAT: &[f32] = &[447.5, 0., 325., 0., 447.5, 240., 0., 0., 1.];
 
 fn estimate(
-    frame: &Mat,
-    xfr: &mut Mat,
+    frame: &mut Mat,
     intrinsics: (&Mat, &Mat),
     detector: &ArucoDetector,
+    draw_marker_info: bool,
 ) -> Result<Option<(f32, f32)>> {
     let mut ids = VectorOfi32::new();
     let mut cn = VectorOfVectorOfPoint2f::new();
@@ -42,7 +45,7 @@ fn estimate(
     ];
 
     let ipv = Mat::from_slice_rows_cols(&ipp, 4, 1)?;
-    detector.detect_markers(&frame, &mut cn, &mut ids, &mut core::no_array())?;
+    detector.detect_markers(frame, &mut cn, &mut ids, &mut core::no_array())?;
 
     if !ids.is_empty() && ids.get(0)? == M_ID {
         println!("{:?}", ids.get(0).unwrap());
@@ -59,35 +62,23 @@ fn estimate(
             SOLVEPNP_IPPE_SQUARE,
         )?;
 
-        draw_frame_axes(xfr, intrinsics.0, intrinsics.1, &rod, &trans, 10., 2)?;
+        if draw_marker_info {
+            draw_frame_axes(frame, intrinsics.0, intrinsics.1, &rod, &trans, 10., 2)?;
 
-        draw_detected_markers(xfr, &cn, &ids, (0., 1., 0.).into())?;
-        arrowed_line(
-            xfr,
-            (320, 240).into(),
-            (320 + 10 * trans[0] as i32, 240 + 10 * trans[1] as i32).into(),
-            (33., 28., 50.).into(),
-            1,
-            16,
-            0,
-            0.1,
-        )?;
+            draw_detected_markers(frame, &cn, &ids, (0., 1., 0.).into())?;
 
-        let mut rot = Mat::new_rows_cols_with_default(3, 3, CV_32FC3, 0.into())?;
-
-        rodrigues(&rod, &mut rot, &mut core::no_array())?;
-
-        put_text(
-            xfr,
-            format!("x: {:.2} y: {:.2} dist: {}", trans[0], trans[1], trans[2]).as_str(),
-            (25, 35).into(),
-            0,
-            0.75,
-            (0., 1., 0.).into(),
-            1,
-            16,
-            false,
-        )?;
+            put_text(
+                frame,
+                format!("x: {:.2} y: {:.2} dist: {}", trans[0], trans[1], trans[2]).as_str(),
+                (25, 35).into(),
+                0,
+                0.75,
+                (0., 1., 0.).into(),
+                1,
+                16,
+                false,
+            )?;
+        }
         // dbg!(trans);
         Ok(Some((trans[0], trans[1])))
     } else {
@@ -95,8 +86,50 @@ fn estimate(
     }
 }
 
+fn frame_to_frame(frame: &Mat) -> ndarray::Array3<u8> {
+    let size = Mat::size(frame).unwrap();
+
+    let res = ndarray::Array3::from_shape_fn(
+        (size.height as usize, size.width as usize, 3),
+        |(height, width, channel)| {
+            let p = *frame
+                .at_2d::<Point3_<u8>>(height as i32, width as i32)
+                .unwrap();
+            match channel {
+                0 => p.x,
+                1 => p.y,
+                2 => p.z,
+                _ => panic!("Invalid channel"),
+            }
+        },
+    );
+
+    res
+}
+
+fn init_video_recordig(frame: &Mat) -> (Encoder, Time) {
+    use std::time::SystemTime;
+    let destination: Locator = PathBuf::from(format!("{:?}.mp4", SystemTime::now())).into();
+    let settings = EncoderSettings::for_h264_yuv420p(
+        frame.mat_size()[1] as usize,
+        frame.mat_size()[0] as usize,
+        true,
+    );
+
+    let encoder = Encoder::new(&destination, settings).expect("failed to create encoder");
+
+    let position = Time::zero();
+
+    (encoder, position)
+}
+
 fn main() -> Result<()> {
+    let args = std::env::args().collect::<Vec<_>>();
+    let draw_marker_info = args.iter().find(|&arg| arg == "--no-marker-info").is_none();
+
+    #[cfg(feature = "pi")]
     let mut uart = Uart::new(19200, Parity::None, 8, 1).unwrap();
+    #[cfg(feature = "pi")]
     uart.set_write_mode(true).unwrap();
 
     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_V4L2)?;
@@ -105,6 +138,9 @@ fn main() -> Result<()> {
 
     let mut frame = Mat::default();
     cam.retrieve(&mut frame, 0)?;
+    println!("{:?}", frame.mat_size()[0]);
+
+    video_rs::init().unwrap();
 
     let dictionary = get_predefined_dictionary(DICT_6X6_50)?;
     let parameters = DetectorParameters::default()?;
@@ -120,20 +156,41 @@ fn main() -> Result<()> {
 
     let mut prev_x = 0.0;
     let mut prev_y = 0.0;
-    // let mut start = Instant::now();
+
     let hz = std::time::Duration::from_secs_f32(0.04);
     const CXOF_PIXEL_SCALING: f32 = 1.76e-3;
 
-    // while cam.grab()? {
+    let (tx, rx) = std::sync::mpsc::sync_channel::<(Mat, Instant)>(1);
+
+    std::thread::spawn(move || {
+        let (frame, _) = rx.recv().unwrap();
+        let mut video_start = Instant::now();
+        let mut _empty_frame_count = 0;
+
+        let (mut encoder, mut position) = init_video_recordig(&frame);
+        loop {
+            let (frame, _) = rx.recv().unwrap();
+            encoder.encode(&frame_to_frame(&frame), &position).unwrap();
+            // position = position.aligned_with(&frame_start.elapsed().into()).add();
+            let duration: Time = Time::from_nth_of_a_second(25);
+            position = position.aligned_with(&duration).add();
+
+            if video_start.elapsed() > std::time::Duration::from_secs(10) {
+                encoder.finish().unwrap();
+                video_start = std::time::Instant::now();
+                (encoder, position) = init_video_recordig(&frame);
+            }
+        }
+    });
+
     loop {
-        let start = Instant::now();
+        let frame_start = Instant::now();
         cam.grab()?;
         cam.retrieve(&mut frame, 0)?;
 
-        let mut res =
-            Mat::new_size_with_default(frame.size()?, CV_32FC3, (0.2, 0.15, 0.13).into())?;
+        let opt_xy = estimate(&mut frame, (&cmat, &cdst), &detector, draw_marker_info)?;
 
-        let opt_xy = estimate(&mut frame, &mut res, (&cmat, &cdst), &detector)?;
+        tx.send((frame.clone(), frame_start)).unwrap();
 
         if let Some((x, y)) = opt_xy {
             // println!(
@@ -146,6 +203,7 @@ fn main() -> Result<()> {
             let y_to_send = ((y - prev_y) / CXOF_PIXEL_SCALING) as i16;
 
             // [0xFE _ xml xmh yml ymh tm quality 0xAA]
+            #[cfg(feature = "pi")]
             uart.write(&[
                 0xFE,
                 0,
@@ -161,17 +219,11 @@ fn main() -> Result<()> {
 
             println!("{}\n{}\n\n", x_to_send, y_to_send);
 
-            // uart.write(buffer)
             prev_x = x;
             prev_y = y;
-            // start = Instant::now();
         }
 
-        std::thread::sleep(hz.saturating_sub(start.elapsed()));
-        // println!("{}", 1. / start.elapsed().as_secs_f32());
-
-        // let duration = start.elapsed();
-        // println!("Time elapsed : {:?}", duration);
+        std::thread::sleep(hz.saturating_sub(frame_start.elapsed()));
+        println!("{}", 1. / frame_start.elapsed().as_secs_f32());
     }
-    Ok(())
 }
